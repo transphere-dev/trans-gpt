523082e1a15f2716ecc1ec52b53478df9d69eb29
John-Oula <johnoula@antratechstudios.com>
2023-07-11 09:35:40 +0800

    auth token redirect to login fix

--- src/app/components/AuthContextWrapper.js
+++ src/app/components/AuthContextWrapper.js
@@ -13,7 +13,6 @@ export const AuthProvider = ({ children }) => {
   const [error, setError] = useState("");
   const [authLoading, setAuthLoading] = useState(true);
   const [loading, setLoading] = useState(false);
-
   const router = useRouter();
   const path = usePathname();
 
@@ -30,7 +29,7 @@ export const AuthProvider = ({ children }) => {
           //   router.push("/chat");
           // }
         } else {
-          const pathname = path.split('/')[1]
+          const pathname = path.split("/")[1];
           if (pathname != "login" || path != "signup" || path != "verify") {
             setAuthLoading(false);
             router.push("/login");
@@ -38,57 +37,53 @@ export const AuthProvider = ({ children }) => {
         }
       } else {
         setAuthLoading(false);
-        // router.push("/login");
+        router.push("/login");
       }
     };
     initUser();
   }, []);
 
-  useEffect(() => {
-    // if (!user && path.split('/')[1] != 'verify' && path.split('/')[1] != 'ResetPassword' && path.split('/')[1] != 'signup' && path.split('/')[1] != 'forgotPassword') {
-    //   router.push("/login");
-    // }
 
+
+  useEffect(() => {
     return () => {
       null;
     };
   }, [user]);
 
-
-
   const login = async (email, password) => {
-    setLoading(true)
+    setLoading(true);
     const data = await loginUser(email, password);
     if (data) {
       // Set the authToken cookie and update the user state
       Cookies.set("authToken", data.token);
       setUser(data.user);
-      setLoading(false)
+      setLoading(false);
       setError(null);
       router.push("/chat");
     } else {
       console.log("User login failed");
-      setLoading(false)
+      setLoading(false);
       setError("User login failed.");
     }
   };
 
   const signup = async (username, email, password, confPass) => {
-    setLoading(true)
-      const data = await registerUser(username,email, password, confPass);
-      if (data) {
-        // Set the authToken cookie and update the user state
-        setError(null);
-        setLoading(false)
+    setLoading(true);
+    const data = await registerUser(username, email, password, confPass);
+    if (data) {
+      // Set the authToken cookie and update the user state
+      setError(null);
+      setLoading(false);
 
-        router.push(`/verify-email?email=${email}`);
-      } else {
-        setLoading(false)
+      router.push(`/verify-email?email=${email}`);
+    } else {
+      setLoading(false);
 
-        console.log("User login failed");
-        setError("Registration failed. Check your password match");
-      }
-    };
+      console.log("User login failed");
+      setError("Registration failed. Check your password match");
+    }
+  };
 
   const logout = () => {
     // Remove the authToken cookie and clear the user state
@@ -98,32 +93,57 @@ export const AuthProvider = ({ children }) => {
 
   const resendVerificationEmail = async (email) => {
     try {
-      setError(null)
-      const response = await fetch('http://192.168.4.62:8080/api/users/resend-verification', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
-  
+      setError(null);
+      const response = await fetch(
+        "http://192.168.4.62:8080/api/users/resend-verification",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
+
       if (response.ok) {
         // Show a success message
       } else {
         // Show an error message
-        setError("Error resending verification email")
+        setError("Error resending verification email");
       }
     } catch (error) {
-      console.error('Error resending verification email:', error);
-      setError(error.message)
+      console.error("Error resending verification email:", error);
+      setError(error.message);
       // Show an error message
     }
   };
-  
-  return (
-    <AuthContext.Provider value={{ user, login, logout, error, signup, loading ,setError,resendVerificationEmail }}>
-      {user && user.emailverified  === false && !authLoading && path.split('/')[1] !='verify' ? <EmailVerify email={user.email} onResendVerification={resendVerificationEmail} /> : !authLoading ? children : <Loading />}
 
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+        error,
+        signup,
+        loading,
+        setError,
+        resendVerificationEmail,
+      }}
+    >
+      {user &&
+      user.emailverified === false &&
+      !authLoading &&
+      path.split("/")[1] != "verify" ? (
+        <EmailVerify
+          email={user.email}
+          onResendVerification={resendVerificationEmail}
+        />
+      ) : !authLoading ? (
+        children
+      ) : (
+        <Loading />
+      )}
     </AuthContext.Provider>
   );
 };
