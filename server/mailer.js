97f7689699a601790296b7eeeb3243aca2c84207
John Oula <johnoula@antratechstudios.com>
2023-10-31 17:46:25 +0800

    Update mailer.js

--- server/mailer.js
+++ server/mailer.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST, // Replace with your email service's SMTP host
   port: process.env.EMAIL_PORT, // Replace with your email service's SMTP port
-  secure: false, // Set to true for port 465, false for other ports
+  secure: true, // Set to true for port 465, false for other ports
   auth: {
     user: process.env.EMAIL_FROM, // Your email service's username
     pass: process.env.EMAIL_PASSWORD, // Your email service's password
