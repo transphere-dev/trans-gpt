6e11a9081ab7430a5a573f4d969f6850eb815d4f
John-Oula <johnoula@antratechstudios.com>
2023-11-02 14:18:02 +0800

    Added path param in dotenv config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 
 // Load environment variables
-require('dotenv').config({path: '../.env'});
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 
 const express = require('express');
 const cors = require('cors');
